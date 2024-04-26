49a76baf25134d987219de498d74078d8ca366d7
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-04-26 20:58:46 +0700

    init

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,29 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
+
+const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:3000/places");
+      const redData = await response.json();
+      setAvailablePlaces(redData.places);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(availablePlaces);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={false}
+      loadingText="Places data is loading..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
